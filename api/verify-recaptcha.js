29159531fc5337e9b0444977dd8fcf28e0e94c10
Fredrir <fhansteen@gmail.com>
2024-02-06 02:06:26 +0100

    new hope

--- api/verify-recaptcha.js
+++ api/verify-recaptcha.js
@@ -1,24 +1,56 @@
-const axios = require('axios');
+const https = require('https');
+const querystring = require('querystring');
 
 export default async (req, res) => {
   const token = req.body.token;
-  const secretKey = process.env.RECAPTCHA_SECRET_KEY; 
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY;
   
-  try {
-    const googleResponse = await axios.post('https://www.google.com/recaptcha/api/siteverify', null, {
-      params: {
-        secret: secretKey,
-        response: token
+  const postData = querystring.stringify({
+    secret: secretKey,
+    response: token
+  });
+
+  const options = {
+    hostname: 'www.google.com',
+    port: 443,
+    path: '/recaptcha/api/siteverify',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  const request = https.request(options, (resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received.
+    resp.on('end', () => {
+      try {
+        const parsedData = JSON.parse(data);
+        if (parsedData.success) {
+          res.status(200).send({ verified: true });
+        } else {
+          res.status(200).send({ verified: false, details: parsedData['error-codes'] });
+        }
+      } catch (error) {
+        console.error('Error parsing response from reCAPTCHA verification:', error);
+        res.status(500).send('Server Error');
       }
     });
-    
-    if (googleResponse.data.success) {
-      res.status(200).send({ verified: true });
-    } else {
-      res.status(200).send({ verified: false, details: googleResponse.data['error-codes'] });
-    }
-  } catch (error) {
+  });
+
+  request.on('error', (error) => {
     console.error('Error verifying reCAPTCHA:', error);
     res.status(500).send('Server Error');
-  }
+  });
+
+  // Write data to request body
+  request.write(postData);
+  request.end();
 };
