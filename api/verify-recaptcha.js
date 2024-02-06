f0808804a0ff43162943ab7335ac9e41ec26f20b
Fredrir <fhansteen@gmail.com>
2024-02-06 01:35:35 +0100

    initial commit

---
+++ api/verify-recaptcha.js
@@ -0,0 +1,24 @@
+import { post } from 'axios';
+
+export default async (req, res) => {
+  const token = req.body.token;
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY; 
+  
+  try {
+    const googleResponse = await post('https://www.google.com/recaptcha/api/siteverify', null, {
+      params: {
+        secret: secretKey,
+        response: token
+      }
+    });
+    
+    if (googleResponse.data.success) {
+      res.status(200).send({ verified: true });
+    } else {
+      res.status(200).send({ verified: false, details: googleResponse.data['error-codes'] });
+    }
+  } catch (error) {
+    console.error('Error verifying reCAPTCHA:', error);
+    res.status(500).send('Server Error');
+  }
+};
